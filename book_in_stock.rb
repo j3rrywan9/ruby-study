cefa5f2514abcc6dd6a46a69869ccabd46cc490a
Jerry Wang <jerry.wang@delphix.com>
2015-10-09 10:34:52 -0700

    add book_in_stock.rb

---
+++ book_in_stock.rb
@@ -0,0 +1,18 @@
+class BookInStock
+
+  attr_reader :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+  end
+
+end
+
+book = BookInStock.new("isbn1", 33.80)
+puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
+book.price = book.price * 0.75 # discount price
+puts "New price = #{book.price}"
+
